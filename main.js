4eacf67cb05739de19228bae15dd8061deeb5928
Artem Aleksandruk <artemaleksandruk@Artems-MacBook-Pro.local>
2018-08-23 12:40:04 -0400

    fix api calls

--- main.js
+++ main.js
@@ -1,45 +1,71 @@
 
 
-function fetchPerson(url) {
-    fetch(url).then(responseAPI => {
-        responseAPI.json().then(responseJson => {
-            let dataObject = responseJson; 
-            console.log(dataObject);
-
-            //I CAN ADD += INNER HTML TO DISPLAY A PERSONS PROFILE???
-            // images come from image folder, need to add your own images.
-            // string of <img src="./images/luke-skywalker.jpg">
-        });
-    });
+async function fetchPerson(url) {
+    const personData =  await fetch(url);
+    const personJson =  await personData.json();
+    return personJson
 }
 
-function buttonEveryPerson() {
-    fetch(`https://swapi.co/api/people/`).then(responseAPI => {
-        responseAPI.json().then(responseJson => {
-            let dataObject = responseJson; 
-
-            for (i=0; i < dataObject.results.length; i++) {
-                let person = dataObject.results[i];
-                document.getElementById('root').innerHTML += `<button class="person-button" onClick="fetchPerson('${person.url}')"> ${person.name} </button>`
-            };
-            
-        });
-    });
+async function getStarwarsPersons() {
+    const personsData =  await fetch(`https://swapi.co/api/people/`);
+    const personsJson =  await personsData.json();
+    return personsJson.results
 }
 
 
-// an idea of how you can build a profile HTML thought javascipt
-function profileHTML(personInfo) {
-    `
-    <div class="profile">
-        <img src...... />
-        <div class="info-box">
-            <H1>...
-            <p> ....
+function profileHTML(person) {
+    const personImage = `${person.name}.jpg`;
+    
+    let personHTML = ` 
+        <div id="${person.name}" class="profile">
+            <img class="profile-image" src="./images/${personImage}" />
+            <div class="profile-box">
+                <p> ${person.name} </p>
+            </div>
         </div>
-    </div>
     `
+    document.getElementById('root').innerHTML += personHTML;
 }
 
+function personDetailsHTML(person) {
+    const personImage = `${person.name}.jpg`;
+    
+    let personHTML = ` 
+        <button onClick="startingFunctionOnPageLoad()"> Back </button>
+        <div id="${person.name}" class="profile">
+            <img class="profile-image" src="./images/${personImage}" />
+            <div class="profile-box">
+                <p> ${person.name} </p>
+            </div>
+        </div>
+    `
+    document.getElementById('root').innerHTML = personHTML;
+}
 
-buttonEveryPerson();
+async function navigateToPersonInfo(url) {
+    const person = await fetchPerson(url);
+    console.log(person)
+    personDetailsHTML(person);
+}
+
+// ALL MY LOGIC GOES HERE
+async function startingFunctionOnPageLoad() {
+    const personsData = await getStarwarsPersons();
+    document.getElementById('root').innerHTML = '';
+
+    for (i=0; i < personsData.length; i++) {
+        profileHTML(personsData[i])
+    };
+
+    for (i=0; i < personsData.length; i++) {
+        const url = personsData[i].url;
+        document.getElementById(personsData[i].name).addEventListener("click", () => navigateToPersonInfo(url));
+        
+    };
+
+
+
+    //LOGIC manulate my DOM
+}
+
+startingFunctionOnPageLoad();
